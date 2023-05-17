c360c25099a2079f82c7615a234d84232d7ef20c
Jewelle Wilson <jewellelenicewilson@gmail.com>
2023-05-17 12:36:03 -0500

    Delete unneeded script

--- redshift/create_external_schema.sql
+++
@@ -1,6 +0,0 @@
-
-create external schema myspectrum_schema 
-from data catalog 
-database 'myspectrum_db' 
-iam_role 'arn:aws:iam::552163833597:role/service-role/AmazonRedshift-CommandsAccessRole-20230512T054146'
-create external database if not exists;
